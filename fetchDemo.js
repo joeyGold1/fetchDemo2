98b52a93ed1857ccb62230399cef41d59e685af5
Joey Gold <joey.gold@softwire.com>
2025-04-24 10:55:50 +0100

    Added dotenv

--- fetchDemo.js
+++ fetchDemo.js
@@ -1,7 +1,12 @@
 import fetch from 'node-fetch';
+import { config } from 'dotenv';
+
+config();
+
+const api_key = process.env.API_KEY;
 
 async function getArrivalsFromTfl(stopPointId) {
-    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals`);
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals?api_key=${api_key}`);
     const responseBody = await response.json();
     return responseBody;
 }
